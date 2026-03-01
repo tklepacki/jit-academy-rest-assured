c2ae3743e3138a68ad1449e26b3e4d6e4a6e1d1f
tklepacki <t.klepacki@thesignalgroup.com>
2026-03-01 23:45:44 +0100

    Adding fix

--- src/test/java/part04/UserTest.java
+++ src/test/java/part04/UserTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThan;
