d562062d3e04bd89e8eddec21ffd2ef94ac2e11a
tklepacki <t.klepacki@thesignalgroup.com>
2026-03-07 10:36:22 +0100

    Part-2

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,16 +1,52 @@
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
+import  static org.hamcrest.Matchers.equalTo;
 
 public class UserTest {
 
     @Test
     public void getUserTest() {
         given().
-            header("x-api-key", "reqres_2f08e5df916845e1b46a779684000666").
+                header("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
         when().
-            get("https://reqres.in/api/users/2").
-        then().
-            statusCode(200).
-            log().all();
+                get("https://reqres.in/api/users/2").
+                then().
+                statusCode(200).
+                log().all();
+    }
+
+    @Test
+    public void getUserListTest() {
+        given().
+                header("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
+        when().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200).
+                log().all();
+    }
+
+    @Test
+    public void getUserNotFoundTest() {
+        given().
+                header("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
+        when().
+                get("https://reqres.in/api/users/999").
+                then().
+                statusCode(404).
+                log().all();
+    }
+
+    @Test
+    public void getUserUnauthorizedTest() {
+        given().
+                header("x-api-key", "invalid_token").
+        when().
+                get("https://reqres.in/api/users/2").
+                then().
+                statusCode(403).
+                body("error", equalTo("invalid_api_key")).
+                log().all();
     }
 }
