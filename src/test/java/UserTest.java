9828ec5a005e2e31873013162244805228d3247f
tklepacki <t.klepacki@thesignalgroup.com>
2026-03-07 11:36:08 +0100

    Part-5

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,10 +1,35 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.given;
-import  static org.hamcrest.Matchers.equalTo;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 public class UserTest {
+
+    private static RequestSpecification requestSpec;
+    private static ResponseSpecification responseSpec;
+
+    @BeforeAll
+    public static void createSpecification() {
+        requestSpec = new RequestSpecBuilder().
+            setBaseUri("https://reqres.in/api/users").
+            addHeader("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
+            build();
+
+        responseSpec = new ResponseSpecBuilder().
+            expectStatusCode(200).
+            expectContentType("application/json;charset=UTF-8").
+            build();
+    }   
 
     @Test
     public void getUserTest() {
@@ -13,19 +38,23 @@ public class UserTest {
         when().
                 get("https://reqres.in/api/users/2").
                 then().
+                body(matchesJsonSchemaInClasspath("schemas/user.json")).
                 statusCode(200).
+                time(lessThan(2L), TimeUnit.SECONDS).
                 log().all();
     }
 
     @Test
     public void getUserListTest() {
         given().
-                header("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
+            spec(requestSpec).
+            queryParam("page", "2").
         when().
-                get("https://reqres.in/api/users?page=2").
-                then().
+        get().
+        then().
+                spec(responseSpec).
                 body(matchesJsonSchemaInClasspath("schemas/userList.json")).
-                statusCode(200).
+                time(lessThan(2L), TimeUnit.SECONDS).
                 log().all();
     }
 
@@ -35,9 +64,8 @@ public class UserTest {
                 header("x-api-key", "reqres_eb68f411937a40c68226fb013febfe14").
         when().
                 get("https://reqres.in/api/users/999").
-                then().
-                statusCode(404).
-                log().all();
+        then().
+                statusCode(404);
     }
 
     @Test
@@ -46,9 +74,8 @@ public class UserTest {
                 header("x-api-key", "invalid_token").
         when().
                 get("https://reqres.in/api/users/2").
-                then().
+        then().
                 statusCode(403).
-                body("error", equalTo("invalid_api_key")).
-                log().all();
+                body("error", equalTo("invalid_api_key"));
     }
 }
